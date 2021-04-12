0a41b112960adcc1a5c1d04c35c4faa195cbc528
Loi Pham <loitpham@gmail.com>
2021-04-11 22:54:36 -0700

    Added event listeners to buttons. Connected to Twitter when clicking on the Twitter icon.

--- Quotes/script.js
+++ Quotes/script.js
@@ -32,9 +32,17 @@ async function getQuote() {
     }
 }
 
-const newQuoteBtnOnClick = () => {
-    getQuote();
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank'); // open twitter link in a new tab
 }
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote()
